a3370e5027088cb534fd001f754c803ae6c951d2
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2024-06-03 15:45:10 -0300

    Actually test Huntington-Hill method

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ using Apportionment
 @testset "Sainte-Laguë method" begin
     votes = [10, 8, 3, 2]
     seats = apportionment(votes, 8)
-    div_min, div_max = divisors(votes, seats)
+    div_min, div_max = divisors(votes, seats, SainteLague())
 
     @test seats == [3, 3, 1, 1]
     @test round.(Int64, prevfloat.(votes / div_min)) == seats
@@ -29,7 +29,7 @@ end
 
 @testset "Huntington-Hill method" begin
     votes = [10, 8, 3]
-    seats = apportionment(votes, 8)
+    seats = apportionment(votes, 8, HuntingtonHill())
 
     @test seats == [4, 3, 1]
 end
